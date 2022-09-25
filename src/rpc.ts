9467cfd87c55832af44c65d33b8db492b1a94318
Dellear <358009775@qq.com>
2022-09-25 23:40:53 +0800

    rpc Middleware

--- src/rpc.ts
+++ src/rpc.ts
@@ -1,9 +1,45 @@
 import { setupHttpClient } from '@midwayjs/rpc';
+import type { Middleware } from '@midwayjs/rpc';
+
+const logger: Middleware = async (
+  ctx,
+  next
+) => {
+  console.log(`<-- ${ctx.req.url}`);
+  await next();
+  console.log(
+    `--> ${ctx.req.url} ${ctx.res}`
+  );
+};
+
+const ErrorHandler: Middleware = async (
+  ctx,
+  next
+) => {
+try {
+  await next();
+} catch (err) {
+  switch (err.status) {
+  case 401:
+      location.href = '/login';
+      break;
+  case 500:
+      alert('Internal Server Error');
+      break;
+  default:
+      alert(
+      `Unknown Error, status: ${err.status}`
+      );
+      break;
+  }
+}
+};
 
 setupHttpClient({
   baseURL:
     process.env.NODE_ENV ===
     'development'
       ? 'http://localhost:3000'
-      : 'https://localhost:3000',
+      : '/',
+    middleware: [logger, ErrorHandler],
 });
